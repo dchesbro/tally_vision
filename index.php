dbef8a30ad83615740f06d595b94be0c6d2d1389
Dwight Chesbro <dwight.chesbro@simplymusic.com>
2019-04-09 23:43:18 -0700

    Added array of songs, modified styles.

--- index.php
+++ index.php
@@ -1,10 +1,264 @@
+<?php
+	
+	// Array of Eurovision Song Contest 2019 songs.
+	$songs = array(
+		array(
+			'country' => 'Albania',
+			'code'    => 'al',
+			'artist'  => 'Jonida Maliqi',
+			'title'   => 'Ktheju tokës'
+		),
+		array(
+			'country' => 'Armenia',
+			'code'    => 'am',
+			'artist'  => 'Srbuk',
+			'title'   => 'Walking Out'
+		),
+		array(
+			'country' => 'Australia',
+			'code'    => 'au',
+			'artist'  => 'Kate Miller-Heidke',
+			'title'   => 'Zero Gravity'
+		),
+		array(
+			'country' => 'Austria',
+			'code'    => 'at',
+			'artist'  => 'Paenda',
+			'title'   => 'Limits'
+		),
+		array(
+			'country' => 'Azerbaijan',
+			'code'    => 'az',
+			'artist'  => 'Chingiz',
+			'title'   => 'Truth'
+		),
+		array(
+			'country' => 'Belarus',
+			'code'    => 'by',
+			'artist'  => 'Zena',
+			'title'   => 'Like It'
+		),
+		array(
+			'country' => 'Belgium',
+			'code'    => 'be',
+			'artist'  => 'Eliot',
+			'title'   => 'Wake Up'
+		),
+		array(
+			'country' => 'Croatia',
+			'code'    => 'hr',
+			'artist'  => 'Roko',
+			'title'   => 'The Dream'
+		),
+		array(
+			'country' => 'Cyprus',
+			'code'    => 'cy',
+			'artist'  => 'Tamta',
+			'title'   => 'Replay'
+		),
+		array(
+			'country' => 'Czech Republic',
+			'code'    => 'cz',
+			'artist'  => 'Lake Malawi',
+			'title'   => 'Friend of a Friend'
+		),
+		array(
+			'country' => 'Denmark',
+			'code'    => 'dk',
+			'artist'  => 'Leonora',
+			'title'   => 'Love Is Forever'
+		),
+		array(
+			'country' => 'Estonia',
+			'code'    => 'ee',
+			'artist'  => 'Victor Crone',
+			'title'   => 'Storm'
+		),
+		array(
+			'country' => 'Finland',
+			'code'    => 'fi',
+			'artist'  => 'Darude feat. Sebastian Rejman',
+			'title'   => 'Look Away'
+		),
+		array(
+			'country' => 'France',
+			'code'    => 'fr',
+			'artist'  => 'Bilal Hassani',
+			'title'   => 'Roi'
+		),
+		array(
+			'country' => 'Georgia',
+			'code'    => 'ge',
+			'artist'  => 'Oto Nemsadze',
+			'title'   => 'Sul tsin iare'
+		),
+		array(
+			'country' => 'Germany',
+			'code'    => 'de',
+			'artist'  => 'S!sters',
+			'title'   => 'Sister'
+		),
+		array(
+			'country' => 'Greece',
+			'code'    => 'gr',
+			'artist'  => 'Katerine Duska',
+			'title'   => 'Better Love'
+		),
+		array(
+			'country' => 'Hungary',
+			'code'    => 'hu',
+			'artist'  => 'Joci Pápai',
+			'title'   => 'Az én apám'
+		),
+		array(
+			'country' => 'Iceland',
+			'code'    => 'is',
+			'artist'  => 'Hatari',
+			'title'   => 'Hatrið mun sigra'
+		),
+		array(
+			'country' => 'Ireland',
+			'code'    => 'ie',
+			'artist'  => 'Sarah McTernan',
+			'title'   => '22'
+		),
+		array(
+			'country' => 'Israel',
+			'code'    => 'il',
+			'artist'  => 'Kobi Marimi',
+			'title'   => 'Home'
+		),
+		array(
+			'country' => 'Italy',
+			'code'    => 'it',
+			'artist'  => 'Mahmood',
+			'title'   => 'Soldi'
+		),
+		array(
+			'country' => 'Latvia',
+			'code'    => 'lv',
+			'artist'  => 'Carousel',
+			'title'   => 'That Night'
+		),
+		array(
+			'country' => 'Lithuania',
+			'code'    => 'lt',
+			'artist'  => 'Jurijus',
+			'title'   => 'Run with the Lions'
+		),
+		array(
+			'country' => 'FYR Macedonia',
+			'code'    => 'mk',
+			'artist'  => 'Tamara Todevska',
+			'title'   => 'Proud'
+		),
+		array(
+			'country' => 'Malta',
+			'code'    => 'mt',
+			'artist'  => 'Michela Pace',
+			'title'   => 'Chameleon'
+		),
+		array(
+			'country' => 'Moldova',
+			'code'    => 'md',
+			'artist'  => 'Anna Odobescu',
+			'title'   => 'Stay'
+		),
+		array(
+			'country' => 'Montenegro',
+			'code'    => 'me',
+			'artist'  => 'D-moll',
+			'title'   => 'Heaven'
+		),
+		array(
+			'country' => 'Netherlands',
+			'code'    => 'nl',
+			'artist'  => 'Duncan Laurence',
+			'title'   => 'Arcade'
+		),
+		array(
+			'country' => 'Norway',
+			'code'    => 'no',
+			'artist'  => 'KEiiNO',
+			'title'   => 'Spirit in the Sky'
+		),
+		array(
+			'country' => 'Poland',
+			'code'    => 'pl',
+			'artist'  => 'Tulia',
+			'title'   => 'Fire of Love (Pali się)'
+		),
+		array(
+			'country' => 'Portugal',
+			'code'    => 'pt',
+			'artist'  => 'Conan Osíris',
+			'title'   => 'Telemóveis'
+		),
+		array(
+			'country' => 'Romania',
+			'code'    => 'ro',
+			'artist'  => 'Ester Peony',
+			'title'   => 'On a Sunday'
+		),
+		array(
+			'country' => 'Russia',
+			'code'    => 'ru',
+			'artist'  => 'Sergey Lazarev',
+			'title'   => 'Scream'
+		),
+		array(
+			'country' => 'San Marino',
+			'code'    => 'sm',
+			'artist'  => 'Serhat',
+			'title'   => 'Say Na Na Na'
+		),
+		array(
+			'country' => 'Serbia',
+			'code'    => 'rs',
+			'artist'  => 'Nevena Božović',
+			'title'   => 'Kruna'
+		),
+		array(
+			'country' => 'Slovenia',
+			'code'    => 'si',
+			'artist'  => 'Zala Kralj & Gašper Šantl',
+			'title'   => 'Sebi'
+		),
+		array(
+			'country' => 'Spain',
+			'code'    => 'es',
+			'artist'  => 'Miki',
+			'title'   => 'La venda'
+		),
+		array(
+			'country' => 'Sweden',
+			'code'    => 'se',
+			'artist'  => 'John Lundvik',
+			'title'   => 'Too Late For Love'
+		),
+		array(
+			'country' => 'Switzerland',
+			'code'    => 'ch',
+			'artist'  => 'Luca Hänni',
+			'title'   => 'She Got Me'
+		),
+		array(
+			'country' => 'United Kingdom',
+			'code'    => 'gb',
+			'artist'  => 'Michael Rice',
+			'title'   => 'Bigger Than Us'
+		)
+	);
+	
+?>
+
 <!doctype html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
 		<meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
 		<link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" rel="stylesheet">
-		<link href="/css/flags/flags.min.css" rel="stylesheet">
+		<link href="./css/flags/flags.min.css" rel="stylesheet">
 		<style>
 			.card {
 				overflow: hidden;
@@ -15,14 +269,39 @@
 				border-radius: 0;
 			}
 			
-			.card-title .flag {
+			.card .card-title .flag {
 				vertical-align: text-bottom;
 			}
 		</style>
 		<title>TALLY VISION</title>
 	</head>
 	<body>
-		<main class="container" id="">
+		<main class="container" id="item-list">
+			<section class="row">
+				<div class="col">
+					
+					<!-- Song list -->
+					<div class="card">
+						<div class="card-header">
+							Eurovision Song Contest 2019
+						</div>
+						<div class="list-group list-group-flush">
+							
+							<?php foreach( $songs as $song ) : ?>
+									
+								<a class="list-group-item list-group-item-action text-truncate" href="#"><?php printf( '%s: %s – "%s"', $song['country'], $song['artist'], $song['title'] ); ?></a>
+								
+							<?php endforeach; ?>
+							
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+		
+		<hr>
+		
+		<main class="container" id="item-ballot">
 			<section class="row">
 				<div class="col">
 					
@@ -32,10 +311,12 @@
 						<!-- Title and description -->
 						<div class="card-body">
 							<h3 class="card-title">
-								<img alt="Country flag icon" class="flag flag-eu" src="/css/flags/blank.gif">
-								Country: "Song Title"
+								<img alt="Country flag icon" class="flag <?php printf( 'flag-%s', $songs[40]['code'] ); ?>" src="./css/flags/blank.gif">
+								
+								<?php printf( '%s: %s – "%s"', $songs[40]['country'], $songs[40]['artist'], $songs[40]['title'] ); ?>
+								
 							</h3>
-							<p class="card-text text-muted">Cast your vote in the categories below and tap submit</p>
+							<p class="card-text text-muted">Cast your vote in the categories below</p>
 						</div>
 						
 						<!-- Ballot form -->
@@ -80,22 +361,22 @@
 									<h5><?php printf( '%d. %s', $key + 1, $category['title'] ); ?></h5>
 									<div class="btn-group btn-group-toggle w-100" data-toggle="buttons">
 										<label class="btn btn-outline-primary">
-											<input autocomplete="off" id="option1" name="options" type="radio"> 0
+											<input autocomplete="off" id="0" name="options" type="radio"> 0
 										</label>
 										<label class="btn btn-outline-primary">
-											<input autocomplete="off" id="option1" name="options" type="radio"> 1
+											<input autocomplete="off" id="1" name="options" type="radio"> 1
 										</label>
 										<label class="btn btn-outline-primary">
-											<input autocomplete="off" id="option1" name="options" type="radio"> 2
+											<input autocomplete="off" id="2" name="options" type="radio"> 2
 										</label>
 										<label class="btn btn-outline-primary">
-											<input autocomplete="off" id="option1" name="options" type="radio"> 3
+											<input autocomplete="off" id="3" name="options" type="radio"> 3
 										</label>
 										<label class="btn btn-outline-primary">
-											<input autocomplete="off" id="option1" name="options" type="radio"> 4
+											<input autocomplete="off" id="4" name="options" type="radio"> 4
 										</label>
 										<label class="btn btn-outline-primary">
-											<input autocomplete="off" id="option1" name="options" type="radio"> 5
+											<input autocomplete="off" id="5" name="options" type="radio"> 5
 										</label>
 									</div>
 									<div class="d-flex justify-content-between">
