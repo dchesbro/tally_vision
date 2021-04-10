0fe6f79e3fbe64e6c704b81872b68733e7ccbbff
Dwight Chesbro <dchesbro@gmail.com>
2021-04-09 19:18:41 -0700

    Formatting.

--- contestants/2019.js
+++ contestants/2019.js
@@ -1,275 +1,275 @@
 var contestants = [
 	{
 		country: 'Albania',
-		code:    'al',
-		artist:  'Jonida Maliqi',
-		title:   'Ktheju tokës',
-		representative: 'Melanie'
+		code: 'al',
+		artist: 'Jonida Maliqi',
+		title: 'Ktheju tokës',
+		representative: 'Melanie',
 	},
 	/* {
 		country: 'Armenia',
-		code:    'am',
-		artist:  'Srbuk',
-		title:   'Walking Out'
+		code: 'am',
+		artist: 'Srbuk',
+		title: 'Walking Out',
 	}, */
 	{
 		country: 'Australia',
-		code:    'au',
-		artist:  'Kate Miller-Heidke',
-		title:   'Zero Gravity',
-		representative: 'Christian'
+		code: 'au',
+		artist: 'Kate Miller-Heidke',
+		title: 'Zero Gravity',
+		representative: 'Christian',
 	},
 	/* {
 		country: 'Austria',
-		code:    'at',
-		artist:  'Paenda',
-		title:   'Limits'
+		code: 'at',
+		artist: 'Paenda',
+		title: 'Limits',
 	}, */
 	{
 		country: 'Azerbaijan',
-		code:    'az',
-		artist:  'Chingiz',
-		title:   'Truth',
-		representative: 'Christian'
+		code: 'az',
+		artist: 'Chingiz',
+		title: 'Truth',
+		representative: 'Christian',
 	},
 	{
 		country: 'Belarus',
-		code:    'by',
-		artist:  'Zena',
-		title:   'Like It',
-		representative: 'Pip'
+		code: 'by',
+		artist: 'Zena',
+		title: 'Like It',
+		representative: 'Pip',
 	},
 	/* {
 		country: 'Belgium',
-		code:    'be',
-		artist:  'Eliot',
-		title:   'Wake Up'
+		code: 'be',
+		artist: 'Eliot',
+		title: 'Wake Up',
 	}, */
 	/* {
 		country: 'Croatia',
-		code:    'hr',
-		artist:  'Roko',
-		title:   'The Dream'
+		code: 'hr',
+		artist: 'Roko',
+		title: 'The Dream',
 	}, */
 	{
 		country: 'Cyprus',
-		code:    'cy',
-		artist:  'Tamta',
-		title:   'Replay',
-		representative: 'Pip'
+		code: 'cy',
+		artist: 'Tamta',
+		title: 'Replay',
+		representative: 'Pip',
 	},
 	{
 		country: 'Czech Republic',
-		code:    'cz',
-		artist:  'Lake Malawi',
-		title:   'Friend of a Friend',
-		representative: 'Dan'
+		code: 'cz',
+		artist: 'Lake Malawi',
+		title: 'Friend of a Friend',
+		representative: 'Dan',
 	},
 	{
 		country: 'Denmark',
-		code:    'dk',
-		artist:  'Leonora',
-		title:   'Love Is Forever',
-		representative: 'Marty'
+		code: 'dk',
+		artist: 'Leonora',
+		title: 'Love Is Forever',
+		representative: 'Marty',
 	},
 	{
 		country: 'Estonia',
-		code:    'ee',
-		artist:  'Victor Crone',
-		title:   'Storm',
-		representative: 'Sarah'
+		code: 'ee',
+		artist: 'Victor Crone',
+		title: 'Storm',
+		representative: 'Sarah',
 	},
 	/* {
 		country: 'Finland',
-		code:    'fi',
-		artist:  'Darude feat. Sebastian Rejman',
-		title:   'Look Away'
+		code: 'fi',
+		artist: 'Darude feat. Sebastian Rejman',
+		title: 'Look Away',
 	}, */
 	{
 		country: 'France',
-		code:    'fr',
-		artist:  'Bilal Hassani',
-		title:   'Roi',
-		representative: 'Pip'
+		code: 'fr',
+		artist: 'Bilal Hassani',
+		title: 'Roi',
+		representative: 'Pip',
 	},
 	/* {
 		country: 'Georgia',
-		code:    'ge',
-		artist:  'Oto Nemsadze',
-		title:   'Sul tsin iare'
+		code: 'ge',
+		artist: 'Oto Nemsadze',
+		title: 'Sul tsin iare',
 	}, */
 	{
 		country: 'Germany',
-		code:    'de',
-		artist:  'S!sters',
-		title:   'Sister',
-		representative: 'Melanie'
+		code: 'de',
+		artist: 'S!sters',
+		title: 'Sister',
+		representative: 'Melanie',
 	},
 	{
 		country: 'Greece',
-		code:    'gr',
-		artist:  'Katerine Duska',
-		title:   'Better Love',
-		representative: 'Zach'
+		code: 'gr',
+		artist: 'Katerine Duska',
+		title: 'Better Love',
+		representative: 'Zach',
 	},
 	/* {
 		country: 'Hungary',
-		code:    'hu',
-		artist:  'Joci Pápai',
-		title:   'Az én apám'
+		code: 'hu',
+		artist: 'Joci Pápai',
+		title: 'Az én apám',
 	}, */
 	{
 		country: 'Iceland',
-		code:    'is',
-		artist:  'Hatari',
-		title:   'Hatrið mun sigra',
-		representative: 'Dan'
+		code: 'is',
+		artist: 'Hatari',
+		title: 'Hatrið mun sigra',
+		representative: 'Dan',
 	},
 	/* {
 		country: 'Ireland',
-		code:    'ie',
-		artist:  'Sarah McTernan',
-		title:   '22'
+		code: 'ie',
+		artist: 'Sarah McTernan',
+		title: '22',
 	}, */
 	{
 		country: 'Israel',
-		code:    'il',
-		artist:  'Kobi Marimi',
-		title:   'Home',
-		representative: 'Zach'
+		code: 'il',
+		artist: 'Kobi Marimi',
+		title: 'Home',
+		representative: 'Zach',
 	},
 	{
 		country: 'Italy',
-		code:    'it',
-		artist:  'Mahmood',
-		title:   'Soldi',
-		representative: 'Dwight'
+		code: 'it',
+		artist: 'Mahmood',
+		title: 'Soldi',
+		representative: 'Dwight',
 	},
 	/* {
 		country: 'Latvia',
-		code:    'lv',
-		artist:  'Carousel',
-		title:   'That Night'
+		code: 'lv',
+		artist: 'Carousel',
+		title: 'That Night',
 	}, */
 	/* {
 		country: 'Lithuania',
-		code:    'lt',
-		artist:  'Jurijus',
-		title:   'Run with the Lions'
+		code: 'lt',
+		artist: 'Jurijus',
+		title: 'Run with the Lions',
 	}, */
 	{
 		country: 'F.Y.R. Macedonia',
-		code:    'mk',
-		artist:  'Tamara Todevska',
-		title:   'Proud',
-		representative: 'Dan'
+		code: 'mk',
+		artist: 'Tamara Todevska',
+		title: 'Proud',
+		representative: 'Dan',
 	},
 	{
 		country: 'Malta',
-		code:    'mt',
-		artist:  'Michela Pace',
-		title:   'Chameleon',
-		representative: 'Dwight'
+		code: 'mt',
+		artist: 'Michela Pace',
+		title: 'Chameleon',
+		representative: 'Dwight',
 	},
 	/* {
 		country: 'Moldova',
-		code:    'md',
-		artist:  'Anna Odobescu',
-		title:   'Stay'
+		code: 'md',
+		artist: 'Anna Odobescu',
+		title: 'Stay',
 	}, */
 	/* {
 		country: 'Montenegro',
-		code:    'me',
-		artist:  'D-moll',
-		title:   'Heaven'
+		code: 'me',
+		artist: 'D-moll',
+		title: 'Heaven',
 	}, */
 	{
 		country: 'Netherlands',
-		code:    'nl',
-		artist:  'Duncan Laurence',
-		title:   'Arcade',
-		representative: 'Dwight'
+		code: 'nl',
+		artist: 'Duncan Laurence',
+		title: 'Arcade',
+		representative: 'Dwight',
 	},
 	{
 		country: 'Norway',
-		code:    'no',
-		artist:  'KEiiNO',
-		title:   'Spirit in the Sky',
-		representative: 'Christian'
+		code: 'no',
+		artist: 'KEiiNO',
+		title: 'Spirit in the Sky',
+		representative: 'Christian',
 	},
 	/* {
 		country: 'Poland',
-		code:    'pl',
-		artist:  'Tulia',
-		title:   'Fire of Love (Pali się)'
+		code: 'pl',
+		artist: 'Tulia',
+		title: 'Fire of Love (Pali się)',
 	}, */
 	/* {
 		country: 'Portugal',
-		code:    'pt',
-		artist:  'Conan Osíris',
-		title:   'Telemóveis'
+		code: 'pt',
+		artist: 'Conan Osíris',
+		title: 'Telemóveis',
 	}, */
 	/* {
 		country: 'Romania',
-		code:    'ro',
-		artist:  'Ester Peony',
-		title:   'On a Sunday'
+		code: 'ro',
+		artist: 'Ester Peony',
+		title: 'On a Sunday',
 	}, */
 	{
 		country: 'Russia',
-		code:    'ru',
-		artist:  'Sergey Lazarev',
-		title:   'Scream',
-		representative: 'Sarah'
+		code: 'ru',
+		artist: 'Sergey Lazarev',
+		title: 'Scream',
+		representative: 'Sarah',
 	},
 	{
 		country: 'San Marino',
-		code:    'sm',
-		artist:  'Serhat',
-		title:   'Say Na Na Na',
-		representative: 'Sarah'
+		code: 'sm',
+		artist: 'Serhat',
+		title: 'Say Na Na Na',
+		representative: 'Sarah',
 	},
 	{
 		country: 'Serbia',
-		code:    'rs',
-		artist:  'Nevena Božović',
-		title:   'Kruna',
-		representative: 'Marty'
+		code: 'rs',
+		artist: 'Nevena Božović',
+		title: 'Kruna',
+		representative: 'Marty',
 	},
 	{
 		country: 'Slovenia',
-		code:    'si',
-		artist:  'Zala Kralj & Gašper Šantl',
-		title:   'Sebi',
-		representative: 'Zach'
+		code: 'si',
+		artist: 'Zala Kralj & Gašper Šantl',
+		title: 'Sebi',
+		representative: 'Zach',
 	},
 	{
 		country: 'Spain',
-		code:    'es',
-		artist:  'Miki',
-		title:   'La venda',
-		representative: 'Dan'
+		code: 'es',
+		artist: 'Miki',
+		title: 'La venda',
+		representative: 'Dan',
 	},
 	{
 		country: 'Sweden',
-		code:    'se',
-		artist:  'John Lundvik',
-		title:   'Too Late For Love',
-		representative: 'Marty'
+		code: 'se',
+		artist: 'John Lundvik',
+		title: 'Too Late For Love',
+		representative: 'Marty',
 	},
 	{
 		country: 'Switzerland',
-		code:    'ch',
-		artist:  'Luca Hänni',
-		title:   'She Got Me',
-		representative: 'Melanie'
+		code: 'ch',
+		artist: 'Luca Hänni',
+		title: 'She Got Me',
+		representative: 'Melanie',
 	},
 	{
 		country: 'United Kingdom',
-		code:    'gb',
-		artist:  'Michael Rice',
-		title:   'Bigger Than Us',
-		representative: 'Sarah'
+		code: 'gb',
+		artist: 'Michael Rice',
+		title: 'Bigger Than Us',
+		representative: 'Sarah',
 	}
 ];
 
