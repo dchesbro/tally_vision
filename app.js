46b34ab102f23de2c7969b5c758e939b25cdb3e0
Dwight Chesbro <dchesbro@gmail.com>
2021-04-13 11:24:35 -0700

    Major revelation -- now defining different socket namespaces for clients and host.

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routerClient = require('./routes/client');
+var routerHost = require('./routes/host');
 
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+
+// ...
+var clients = io.of('/client');
+var host = io.of('/host');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,8 +38,8 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routerClient);
+app.use('/host', routerHost);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -57,11 +61,147 @@ app.use(function(err, req, res, next) {
 app.set('categories', require('./categories'));
 app.set('contestants', require('./contestants/2021'));
 
-var appUserCount = 0;
-var appVoting = null;
+var appBallot = [];
+var appUsers = [];
 
 // ...
+function hostUsers() {
+  /* for (let [id, socket] of clients.sockets) {
+    appUsers.push({
+      username: socket.username,
+    });
+  } */
+
+  appUsers.sort(function(a, b) {
+    var result = 0;
+    
+    if (a.username > b.username) {
+      result = 1;
+    }
+    if (a.username < b.username) {
+      result = -1;
+    }
+    
+    return result;
+  });
+
+  host.emit('hostUsers', appUsers);
+}
+
+function randomID() {
+  return Math.floor(10000 + Math.random() * 90000);
+}
+
+// ...
+clients.use(function(socket, next) {
+  var auth = socket.handshake.auth;
+
+  if (auth.client) {
+    if (auth.sessionID) {
+      var session = sessionFind(auth.sessionID);
+
+      if (session) {
+        socket.sessionID = sessionID;
+        socket.userID = session.userID;
+        socket.username = session.username;
+        
+        return next();
+      }
+    }
+
+    if (!auth.username) {
+      return next(new Error('missing username'));
+    }
+
+    socket.sessionID = randomID();
+    socket.userID = randomID();
+    socket.username = auth.username;
+  }
+
+  next();
+});
+
+clients.on('connection', function(socket) {
+  // ...
+  appUsers.push({
+    connected: true,
+    username: socket.username,
+  });
+
+  socket.emit('clientJoin', {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+    username: socket.username,
+  });
+
+  hostUsers();
+});
+
+// ...
+host.on('connection', function(socket) {
+  hostUsers();
+});
+
+/* ...
+io.use(function(socket, next) {
+  var auth = socket.handshake.auth;
+
+  if (auth.client) {
+    if (auth.sessionID) {
+      var session = sessionFind(auth.sessionID);
+
+      if (session) {
+        socket.sessionID = sessionID;
+        socket.userID = session.userID;
+        socket.username = session.username;
+        
+        return next();
+      }
+    }
+
+    if (!auth.username) {
+      return next(new Error('missing username'));
+    }
+
+    socket.sessionID = randomID();
+    socket.userID = randomID();
+    socket.username = auth.username;
+  }
+
+  next();
+});
+
 io.on('connection', function(socket) {
+  if (socket.username) {
+
+  }
+
+  appUsers = [];
+
+  appUsers.push({
+    id: socket.id,
+    username: socket.username,
+  });
+  appUsers.sort(function(a, b) {
+    var result = 0;
+    
+    if (a.username > b.username) {
+      result = 1;
+    }
+    if (a.username < b.username) {
+      result = -1;
+    }
+    
+    return result;
+  });
+  socket.emit('appUsers', appUsers);
+
+  socket.emit('clientJoin', {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+    username: socket.username,
+  });
+
   // ...
   socket.on('clientGraham', function(args) {
     console.log(args);
@@ -91,7 +231,7 @@ io.on('connection', function(socket) {
   socket.on('hostOpen', function(args) {
     console.log(args);
   });
-});
+});*/
 
 module.exports = {
   app: app,
