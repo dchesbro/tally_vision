1bff4a745e0bbce103dd9252fa7229daeba1ef1d
Dwight Chesbro <dchesbro@gmail.com>
2021-04-29 00:24:12 -0700

    Basic structure beginning to take shape.

--- app.js
+++ app.js
@@ -10,31 +10,38 @@ var routerHost = require('./routes/host');
 
 var app = express();
 var server = require('http').createServer(app);
+
 var io = require('socket.io')(server);
+var client = io.of('/client');
+var host = io.of('/host');
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./' + new Date().toDateString() + '.db');
 
 // ...
-var clients = io.of('/client');
-var host = io.of('/host');
+var appBallot = {};
+var appCategories = require('./categories');
+var appContestants = require('./contestants/2021');
+var appVoters = [];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// pass socket to response
-app.use(function(req, res, next) {
-  res.io = io;
-  next();
-});
+app.set('categories', appCategories);
+app.set('contestants', appContestants);
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false,
+}));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
+  indentedSyntax: false,
+  sourceMap: true,
+  src: path.join(__dirname, 'public'),
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -58,180 +65,204 @@ app.use(function(err, req, res, next) {
 });
 
 // ...
-app.set('categories', require('./categories'));
-app.set('contestants', require('./contestants/2021'));
+client.use(function(socket, next) {
+  var auth = socket.handshake.auth;
 
-var appBallot = [];
-var appUsers = [];
+  if (!auth.name) {
+    return next(new Error('missing name'));
+  }
+
+  socket.name = auth.name.toLowerCase();
+
+  next();
+});
 
 // ...
-function hostUsers() {
-  /* for (let [id, socket] of clients.sockets) {
-    appUsers.push({
-      username: socket.username,
+client.on('connection', function(socket) {
+  clientConnect(socket);
+
+  socket.on('clientBallotSubmit', function(scores) {
+    dbInsertVote(socket, scores);
+  });
+  
+  socket.on('disconnect', function() {
+    clientDisconnect(socket);
+  });
+});
+
+// ...
+function clientConnect(socket) {
+  var [i, voter] = voterGet(socket);
+
+  if (isEmpty(voter)) {
+    appVoters.push({
+      connected: true,
+      name: socket.name,
     });
-  } */
 
-  appUsers.sort(function(a, b) {
-    var result = 0;
-    
-    if (a.username > b.username) {
-      result = 1;
-    }
-    if (a.username < b.username) {
-      result = -1;
-    }
-    
-    return result;
-  });
-
-  host.emit('hostUsers', appUsers);
-}
-
-function randomID() {
-  return Math.floor(10000 + Math.random() * 90000);
-}
-
-// ...
-clients.use(function(socket, next) {
-  var auth = socket.handshake.auth;
-
-  if (auth.client) {
-    if (auth.sessionID) {
-      var session = sessionFind(auth.sessionID);
-
-      if (session) {
-        socket.sessionID = sessionID;
-        socket.userID = session.userID;
-        socket.username = session.username;
-        
-        return next();
-      }
-    }
-
-    if (!auth.username) {
-      return next(new Error('missing username'));
-    }
-
-    socket.sessionID = randomID();
-    socket.userID = randomID();
-    socket.username = auth.username;
+    voterSort();
+  } else {
+    appVoters[i].connected = true;
   }
 
-  next();
-});
+  socket.emit('clientConnect', socket.name);
 
-clients.on('connection', function(socket) {
-  // ...
-  appUsers.push({
-    connected: true,
-    username: socket.username,
-  });
+  if (!isEmpty(appBallot)) {
+    socket.emit('appBallotOpen', appBallot);
+  }
 
-  socket.emit('clientJoin', {
-    sessionID: socket.sessionID,
-    userID: socket.userID,
-    username: socket.username,
-  });
-
-  hostUsers();
-});
+  hostVoters();
+}
 
 // ...
+function clientDisconnect(socket) {
+  var [i, voter] = voterGet(socket);
+
+  if (!isEmpty(voter)) {
+    appVoters[i].connected = false;
+  }
+
+  socket.emit('clientDisconnect', socket.name);
+
+  hostVoters();
+}
+
+
+
+//...
 host.on('connection', function(socket) {
-  hostUsers();
-});
+  hostConnect();
 
-/* ...
-io.use(function(socket, next) {
-  var auth = socket.handshake.auth;
+  socket.on('hostBallotClose', function() {
+    appBallot = {};
 
-  if (auth.client) {
-    if (auth.sessionID) {
-      var session = sessionFind(auth.sessionID);
-
-      if (session) {
-        socket.sessionID = sessionID;
-        socket.userID = session.userID;
-        socket.username = session.username;
-        
-        return next();
-      }
-    }
-
-    if (!auth.username) {
-      return next(new Error('missing username'));
-    }
-
-    socket.sessionID = randomID();
-    socket.userID = randomID();
-    socket.username = auth.username;
-  }
-
-  next();
-});
-
-io.on('connection', function(socket) {
-  if (socket.username) {
-
-  }
-
-  appUsers = [];
-
-  appUsers.push({
-    id: socket.id,
-    username: socket.username,
+    client.emit('appBallotClose');
+    host.emit('appBallotClose');
   });
-  appUsers.sort(function(a, b) {
-    var result = 0;
-    
-    if (a.username > b.username) {
-      result = 1;
+
+  socket.on('hostBallotOpen', function(i) {
+    appBallot = appContestants[i];
+
+    client.emit('appBallotOpen', appBallot);
+    host.emit('appBallotOpen', appBallot);
+  });
+});
+
+// ...
+function hostConnect() {
+  hostBallot();
+  hostVoters();
+}
+
+// ...
+function hostBallot() {
+  if (!isEmpty(appBallot)) {
+    host.emit('appBallotOpen', appBallot);
+  } else {
+    host.emit('appBallotClose');
+  }
+}
+
+// ...
+function hostVoters() {
+  if (!isEmpty(appVoters)) {
+    host.emit('hostVoters', appVoters);
+  }
+}
+
+
+
+// ...
+function dbCreateTables() {
+  var columns = ``;
+  var sql = ``;
+
+  columns = appCategories.map(function(category) {
+    return `category_` + category.title + ` INTEGER NOT NULL`;
+  }).join(`,`);
+
+  sql += `CREATE TABLE IF NOT EXISTS votes (`;
+  sql += `uid INTEGER PRIMARY KEY AUTOINCREMENT,`;
+  sql += `voter_name TEXT NOT NULL,`;
+  sql += `contestant_code TEXT NOT NULL,` + columns + `);`;
+
+  db.run(sql, function(err) {});
+}
+
+dbCreateTables();
+
+// ...
+function dbGetVote(name, code, callback) {
+  var sql = ``;
+
+  sql = `SELECT uid id FROM votes WHERE voter_name=?`;
+
+  db.get(sql, [name], function(err, row) {
+    if (!err) {
+      callback(row.id);
     }
-    if (a.username < b.username) {
-      result = -1;
+  });
+}
+
+// ...
+function dbInsertVote(socket, scores) {
+  var columns = ``;
+  var placeholders = ``;
+  var sql = ``;
+  var values = [socket.name, appBallot.code].concat(scores);
+
+  columns = appCategories.map(function(category) {
+    return `category_` + category.title;
+  }).join(`,`);
+
+  placeholders = appCategories.map(function() {
+    return `?`;
+  }).join(`,`);
+
+  sql += `INSERT INTO votes (`;
+  sql += `voter_name,`;
+  sql += `contestant_code,` + columns + `) VALUES (?,?,` + placeholders + `);`;
+
+  db.run(sql, values, function(err) {
+    if (!err) {
+      socket.emit('appVoted', scores.reduce(function(a, b) {
+        return a + b;
+      }, 0));
+    }
+  });
+}
+
+// ...
+function isEmpty(a) {
+  return Object.keys(a).length === 0;
+}
+
+// ...
+function voterGet(socket) {
+  var result = [null, {}];
+
+  for ([i, voter] of appVoters.entries()) {
+    if (voter.name === socket.name) {
+      result = [i, voter];
+      break;
+    }
+  }
+
+  return result;
+}
+
+// ...
+function voterSort() {
+  appVoters.sort(function(a, b) {
+    var result = -1;
+    
+    if (a.name > b.name) {
+      result = 1;
     }
     
     return result;
   });
-  socket.emit('appUsers', appUsers);
-
-  socket.emit('clientJoin', {
-    sessionID: socket.sessionID,
-    userID: socket.userID,
-    username: socket.username,
-  });
-
-  // ...
-  socket.on('clientGraham', function(args) {
-    console.log(args);
-  });
-
-  // ...
-  socket.on('clientJoin', function(args) {
-    console.log(args);
-  });
-
-  // ...
-  socket.on('clientVote', function(args) {
-    console.log(args);
-  });
-
-  // ...
-  socket.on('hostClose', function(args) {
-    console.log(args);
-  });
-
-  // ...
-  socket.on('hostJoin', function(args) {
-    console.log(args);
-  });
-
-  // ...
-  socket.on('hostOpen', function(args) {
-    console.log(args);
-  });
-});*/
+}
 
 module.exports = {
   app: app,
