fc46ebcf2c4d7c745df8db0074fcb23823697e4f
Dwight Chesbro <dchesbro@gmail.com>
2021-06-04 10:37:08 -0700

    Update 2021.js

--- contestants/2021.js
+++ contestants/2021.js
@@ -4,273 +4,273 @@ var contestants = [
     code: 'al',
     artist: 'Anxhela Peristeri',
     title: 'Karma',
-    representative: '',
+    representative: 'Dwight',
   },
-  {
+  /* {
     country: 'Australia',
     code: 'au',
     artist: 'Montaigne',
     title: 'Technicolor',
     representative: '',
-  },
-  {
+  }, */
+  /* {
     country: 'Austria',
     code: 'at',
     artist: 'Vincent Bueno',
     title: 'Amen',
     representative: '',
-  },
+  }, */
   {
     country: 'Azerbaijan',
     code: 'az',
     artist: 'Efendi',
     title: 'Mata Hari',
-    representative: '',
+    representative: 'Melanie',
   },
   {
     country: 'Belgium',
     code: 'be',
     artist: 'Hooverphonic',
     title: 'The Wrong Place',
-    representative: '',
+    representative: 'Pip',
   },
   {
     country: 'Bulgaria',
     code: 'bg',
     artist: 'VICTORIA',
     title: 'Growing Up Is Getting Old',
-    representative: '',
+    representative: 'Pip',
   },
-  {
+  /* {
     country: 'Croatia',
     code: 'hr',
     artist: 'Albina',
     title: 'Tick-Tock',
     representative: '',
-  },
+  }, */
   {
     country: 'Cyprus',
     code: 'cy',
     artist: 'Elena Tsagrinou',
     title: 'El Diablo',
-    representative: '',
+    representative: 'Sarah',
   },
-  {
+  /* {
     country: 'Czech Republic',
     code: 'cz',
     artist: 'Benny Cristo',
     title: 'omaga',
     representative: '',
-  },
-  {
+  }, */
+  /* {
     country: 'Denmark',
     code: 'dk',
     artist: 'Fyr Og Flamme',
     title: 'Øve Os På Hinanden',
     representative: '',
-  },
-  {
+  }, */
+  /* {
     country: 'Estonia',
     code: 'ee',
     artist: 'Uku Suviste',
     title: 'The Lucky One',
     representative: '',
-  },
+  }, */
   {
     country: 'Finland',
     code: 'fi',
     artist: 'Blind Channel',
     title: 'Dark Side',
-    representative: '',
+    representative: 'Christian',
   },
   {
     country: 'France',
     code: 'fr',
     artist: 'Barbara Pravi',
     title: 'Voilà',
-    representative: '',
+    representative: 'Melanie',
   },
-  {
+  /* {
     country: 'Georgia',
     code: 'ge',
     artist: 'Tornike Kipiani',
     title: 'You',
     representative: '',
-  },
+  }, */
   {
     country: 'Germany',
     code: 'de',
     artist: 'Jendrik',
     title: 'I Don\'t Feel Hate',
-    representative: '',
+    representative: 'Dwight',
   },
   {
     country: 'Greece',
     code: 'gr',
     artist: 'Stefania',
     title: 'Last Dance',
-    representative: '',
+    representative: 'Zach',
   },
   {
     country: 'Iceland',
     code: 'is',
     artist: 'Daði og Gagnamagnið',
     title: '10 Years',
-    representative: '',
+    representative: 'Sarah',
   },
-  {
+  /* {
     country: 'Ireland',
     code: 'ie',
     artist: 'Lesley Roy',
     title: 'MAPS',
     representative: '',
-  },
+  }, */
   {
     country: 'Israel',
     code: 'il',
     artist: 'Eden Alene',
     title: 'Set Me Free',
-    representative: '',
+    representative: 'Melanie',
   },
   {
     country: 'Italy',
     code: 'it',
     artist: 'Måneskin',
     title: 'Zitti E Buoni',
-    representative: '',
+    representative: 'Marty',
   },
-  {
+  /* {
     country: 'Latvia',
     code: 'lv',
     artist: 'Samantha Tïna',
     title: 'The Moon Is Rising',
     representative: '',
-  },
+  }, */
   {
     country: 'Lithuania',
     code: 'lt',
     artist: 'The Roop',
     title: 'Discoteque',
-    representative: '',
+    representative: 'Dogs',
   },
   {
     country: 'Malta',
     code: 'mt',
     artist: 'Destiny',
     title: 'Je Me Casse',
-    representative: '',
+    representative: 'Zach',
   },
   {
     country: 'Moldova',
     code: 'md',
     artist: 'Natalia Gordienko',
     title: 'SUGAR',
-    representative: '',
+    representative: 'Marty',
   },
-  {
+  /* {
     country: 'North Macedonia',
     code: 'mk',
     artist: 'Vasil',
     title: 'Here I Stand',
     representative: '',
-  },
+  }, */
   {
     country: 'Norway',
     code: 'no',
     artist: 'TIX',
     title: 'Fallen Angel',
-    representative: '',
+    representative: 'Marty',
   },
-  {
+  /* {
     country: 'Poland',
     code: 'pl',
     artist: 'RAFAŁ',
     title: 'The Ride',
     representative: '',
-  },
+  }, */
   {
     country: 'Portugal',
     code: 'pt',
     artist: 'The Black Momba',
     title: 'Love Is On My Side',
-    representative: '',
+    representative: 'Dan',
   },
-  {
+  /* {
     country: 'Romania',
     code: 'ro',
     artist: 'ROXEN',
     title: 'Amnesia',
     representative: '',
-  },
+  }, */
   {
     country: 'Russia',
     code: 'ru',
     artist: 'Manizha',
     title: 'Russian Woman',
-    representative: '',
+    representative: 'Dwight',
   },
   {
     country: 'San Marino',
     code: 'sm',
     artist: 'Senhat',
     title: 'Adrenalina',
-    representative: '',
+    representative: 'Dan',
   },
   {
     country: 'Serbia',
     code: 'rs',
     artist: 'Hurricane',
     title: 'Loco Loco',
-    representative: '',
+    representative: 'Christian',
   },
-  {
+  /* {
     country: 'Slovenia',
     code: 'si',
     artist: 'Ana Soklič',
     title: 'Amen',
     representative: '',
-  },
+  }, */
   {
     country: 'Spain',
     code: 'es',
     artist: 'Blas Cantó',
     title: 'Voy A Quedarme',
-    representative: '',
+    representative: 'Dan',
   },
   {
     country: 'Sweden',
     code: 'se',
     artist: 'Tusse',
     title: 'Voices',
-    representative: '',
+    representative: 'Sarah',
   },
   {
     country: 'Switzerland',
     code: 'ch',
     artist: 'Gjon\'s Tears',
     title: 'Tout l\'Univers',
-    representative: '',
+    representative: 'Pip',
   },
   {
     country: 'The Netherlands',
     code: 'nl',
     artist: 'Jeangu Macrooy',
     title: 'Birth Of A New Age',
-    representative: '',
+    representative: 'Zach',
   },
   {
     country: 'Ukraine',
     code: 'ua',
     artist: 'Go_A',
     title: 'Shum',
-    representative: '',
+    representative: 'Dogs',
   },
   {
     country: 'United Kingdom',
     code: 'gb',
     artist: 'James Newman',
     title: 'Embers',
-    representative: '',
+    representative: 'Christian',
   },
 ];
 
